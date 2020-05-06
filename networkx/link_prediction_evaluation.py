cdaea5e7a15efae7a6bfaccc6d04fd95f872e90a
Daya <mingshan.jia@gmail.com>
2020-05-06 18:41:59 +1000

    add another sampling method

--- networkx/link_prediction_evaluation.py
+++ networkx/link_prediction_evaluation.py
@@ -72,6 +72,78 @@ def link_pred_app(G, repeat=1, old_pct=0.5):
     #res.append(dgr)
     return res
 
+
+# sample 5K as G, then split into G_old and G_new
+def link_pred_sample_app_2(G, repeat=1, sample_time=10, sample_size=5000, old_pct=0.5):
+    print('sample time : %d' % sample_time)     
+    print('repeat split time: %d' % repeat)
+    print('old edges percentage: %.1f' % old_pct)
+    res = []
+    rg = 0 #random guess
+    cn = 0
+    aa = 0
+    ra = 0
+    clo1 = 0
+    clo2 = 0
+
+    e_all = list(G.edges(data=True))
+           
+    for i in range(0, sample_time):
+        print('sample %d' % i)       
+        random_edge = random.choice(e_all)
+        random_node = random_edge[0]
+        sample_nodes = get_sample_nodes(G, random_node, sample_size)        
+        print("sampling done")
+        G_sampled = G.subgraph(sample_nodes)
+        e_all = list(G_sampled.edges(data=True))
+        k = round(len(e_all) * old_pct)  
+
+        for n in range(0, repeat):
+            print('repeat = %d' % n)
+
+            if repeat == 1:
+                e_all = sorted(e_all, key=lambda t: t[2].get('sec'))  # for dataset with timestamp
+            else:
+                random.shuffle(e_all)
+
+            e_old = e_all[: k]
+            e_new = e_all[k:]
+            G_old = nx.DiGraph()
+            G_new = nx.DiGraph()
+            G_old.add_edges_from(e_old)
+            G_new.add_edges_from(e_new)
+
+            dict_ce = nx.closure(G_old)
+
+            rg += nx.random_guess(G_old, G_new)
+            print('rg: %.3f' % (rg))
+            cn += nx.perform_link_prediction(G_old, G_new, 'cn', dict_ce)
+            print('cn: %.3f' % (cn))
+            aa += nx.perform_link_prediction(G_old, G_new, 'aa', dict_ce)
+            print('aa: %.3f' % (aa))
+            ra += nx.perform_link_prediction(G_old, G_new, 'ra', dict_ce)
+            print('ra: %.3f' % (ra))
+            clo1 += nx.perform_link_prediction(G_old, G_new, 'clo1', dict_ce)
+            print('clo1: %.3f' % (clo1))
+            clo2 += nx.perform_link_prediction(G_old, G_new, 'clo2', dict_ce)
+            print('clo2: %.3f' % (clo2))
+
+    rg /= (repeat * sample_time)
+    cn /= (repeat * sample_time)
+    aa /= (repeat * sample_time)
+    ra /= (repeat * sample_time)
+    clo1 /= (repeat * sample_time)
+    clo2 /= (repeat * sample_time)
+
+    res.append(rg)
+    res.append(cn)
+    res.append(aa)
+    res.append(ra)
+    res.append(clo1)
+    res.append(clo2)
+    return res
+
+
 # for very large networks, nodes > 10K. input graph G, repeat = 1 for dataset with timestamp, set repeat > 1 for dataset without timestamp
 # old_pct is the the percentage of old edges, based on which we predict new edges
 def link_pred_sample_app(G, repeat=1, sample_time=5, sample_size=1000, old_pct=0.5):
