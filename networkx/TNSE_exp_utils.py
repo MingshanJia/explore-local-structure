0de4cb4117a1eaf5ec1c0cccff5614578c86c432
Mingshan <mingshan.jia@gmail.com>
2020-08-24 18:16:24 +1000

    add correlation of closeness centrality with four coefs.

--- networkx/TNSE_exp_utils.py
+++ networkx/TNSE_exp_utils.py
@@ -1,6 +1,6 @@
 import networkx as nx
 
-__all__ = ['degree_coefs_corr', 'get_four_coefs']
+__all__ = ['degree_coefs_corr', 'get_four_coefs', 'closeness_coefs_corr']
 
 # if the last chunk is less than bin width, add it to the second to last bin
 def chunks(lst, n):
@@ -31,16 +31,37 @@ def degree_coefs_corr(G):
         avg_iquad = nx.average_inner_quad_co(G, node_list)
         avg_oquad = nx.average_outer_quad_co(G, node_list)
 
-        if avg_degree > 1:
-            x.append(avg_degree)
-        if avg_clustering > 0:
-            clu.append(avg_clustering)
-        if avg_closure > 0:
-            clo.append(avg_closure)
-        if avg_iquad > 0:
-            iquad.append(avg_iquad)
-        if avg_oquad > 0:
-            oquad.append(avg_oquad)
+        x.append(avg_degree)
+        clu.append(avg_clustering)
+        clo.append(avg_closure)
+        iquad.append(avg_iquad)
+        oquad.append(avg_oquad)
+    return x, clu, clo, iquad, oquad
+
+
+def closeness_coefs_corr(G):
+    closeness_list = list(nx.closeness_centrality(G).items())
+    sorted_list = [(k,v) for k, v in sorted(closeness_list, key=lambda item: item[1])]
+    bin_width = len(sorted_list) // 10
+    bin_it = chunks(sorted_list, bin_width)
+    x = []
+    clu = []
+    clo = []
+    iquad = []
+    oquad = []
+    for one_bin in bin_it:
+        node_list = [n for n, _ in one_bin]
+        avg_closeness = sum(d for _, d in one_bin) / len(one_bin)
+        avg_clustering = nx.average_clustering(G, node_list)
+        avg_closure = nx.average_closure(G, node_list)
+        avg_iquad = nx.average_inner_quad_co(G, node_list)
+        avg_oquad = nx.average_outer_quad_co(G, node_list)
+
+        x.append(avg_closeness)
+        clu.append(avg_clustering)
+        clo.append(avg_closure)
+        iquad.append(avg_iquad)
+        oquad.append(avg_oquad)
     return x, clu, clo, iquad, oquad
 
 
